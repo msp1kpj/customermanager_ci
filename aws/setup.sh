7b260ea4ac88066aa57b3655edbc5d9efcee9308
Ken John <kenjohn@MacBook-Pro.home.kjsbh.com>
2019-02-03 17:37:44 -0600

    Updated setup to create config file and append files to setup

--- aws/setup.sh
+++ aws/setup.sh
@@ -4,22 +4,32 @@ cd /tmp/customermanager_ci/
 sudo rm -rf /opt/bitnami/apache2/htdocs/*
 sudo cp -R ./myapp/* /opt/bitnami/apache2/htdocs/
 
-echo '' >> /opt/bitnami/scripts/setenv.sh
-echo '#### Application Environment Variables ####' >> /opt/bitnami/scripts/setenv.sh
-echo 'db_pass=$(cat /home/bitnami/bitnami_application_password)' >> /opt/bitnami/scripts/setenv.sh
-echo 'DATABASE_SERVER=localhost' >> /opt/bitnami/scripts/setenv.sh
-echo 'export DATABASE_SERVER' >> /opt/bitnami/scripts/setenv.sh
-echo 'DATABASE_NAME=hpcustomer' >> /opt/bitnami/scripts/setenv.sh
-echo 'export DATABASE_NAME' >> /opt/bitnami/scripts/setenv.sh
-echo 'DATABASE_USER=root' >> /opt/bitnami/scripts/setenv.sh
-echo 'export DATABASE_USER' >> /opt/bitnami/scripts/setenv.sh
-echo 'DATABASE_PASSWORD=$db_pass' >> /opt/bitnami/scripts/setenv.sh
-echo 'export DATABASE_PASSWORD' >> /opt/bitnami/scripts/setenv.sh
+echo '' > /opt/bitnami/scripts/setenv_app.sh
+echo '#### Application Environment Variables ####' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'db_pass=$(cat /home/bitnami/bitnami_application_password)' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'DATABASE_SERVER=localhost' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'export DATABASE_SERVER' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'DATABASE_NAME=hpcustomer' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'export DATABASE_NAME' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'DATABASE_USER=root' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'export DATABASE_USER' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'DATABASE_PASSWORD=$db_pass' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'export DATABASE_PASSWORD' >> /opt/bitnami/scripts/setenv_app.sh
+
+LINE='. /opt/bitnami/scripts/setenv_app.sh'
+FILE=/opt/bitnami/scripts/setenv.sh
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
 
 #Allow Database variable from environment
+echo '' > /opt/bitnami/php/etc/environment_app.conf
 echo 'env[DATABASE_SERVER] = $DATABASE_SERVER' >> /opt/bitnami/php/etc/environment.conf
 echo 'env[DATABASE_NAME] = $DATABASE_NAME' >> /opt/bitnami/php/etc/environment.conf
 echo 'env[DATABASE_USER] = $DATABASE_USER' >> /opt/bitnami/php/etc/environment.conf
 echo 'env[DATABASE_PASSWORD] = $DATABASE_PASSWORD' >> /opt/bitnami/php/etc/environment.conf
+
+LINE='include=/opt/bitnami/php/etc/environment_app.conf'
+FILE=/opt/bitnami/php/etc/environment_app.conf
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
 
