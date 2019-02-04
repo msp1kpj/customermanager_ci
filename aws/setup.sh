1b6f4bcc39f7ce748d47bc23e205cf42e9ef7c88
Ken John <kenjohn@MacBook-Pro.home.kjsbh.com>
2019-02-03 19:59:37 -0600

    Added the Production environment flag

--- aws/setup.sh
+++ aws/setup.sh
@@ -16,6 +16,9 @@ echo 'DATABASE_USER=root' >> /opt/bitnami/scripts/setenv_app.sh
 echo 'export DATABASE_USER' >> /opt/bitnami/scripts/setenv_app.sh
 echo 'DATABASE_PASSWORD=$db_pass' >> /opt/bitnami/scripts/setenv_app.sh
 echo 'export DATABASE_PASSWORD' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'CI_ENV=production' >> /opt/bitnami/scripts/setenv_app.sh
+echo 'export CI_ENV' >> /opt/bitnami/scripts/setenv_app.sh
+
 
 LINE='. /opt/bitnami/scripts/setenv_app.sh'
 FILE=/opt/bitnami/scripts/setenv.sh
@@ -28,6 +31,7 @@ echo 'env[DATABASE_SERVER] = $DATABASE_SERVER' >> /opt/bitnami/php/etc/environme
 echo 'env[DATABASE_NAME] = $DATABASE_NAME' >> /opt/bitnami/php/etc/environment_app.conf
 echo 'env[DATABASE_USER] = $DATABASE_USER' >> /opt/bitnami/php/etc/environment_app.conf
 echo 'env[DATABASE_PASSWORD] = $DATABASE_PASSWORD' >> /opt/bitnami/php/etc/environment_app.conf
+echo 'env[CI_ENV] = $CI_ENV' >> /opt/bitnami/php/etc/environment_app.conf
 
 LINE='include=/opt/bitnami/php/etc/environment_app.conf'
 FILE=/opt/bitnami/php/etc/environment.conf
