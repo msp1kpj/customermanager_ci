6e2c5e3d24de87fa94dc613ee106fa714cf16b58
Ken John <kenjohn@MacBook-Pro.home.kjsbh.com>
2019-02-03 18:30:22 -0600

    Renamed models

---
+++ myapp/application/models/Account_dt.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account_dt extends Account_model implements DatatableModel
+{
+    public function appendToSelectStr() {
+            return array(
+                'city_state_zip' => 'concat(c1.state, \'  \', c1.city, \'  \', c1.postalCode)'
+            );
+
+    }
+
+    public function fromTableStr() {
+        return 'customer c1';
+    }
+
+
+    public function joinArray(){
+        return NULL;
+    }
+
+    public function whereClauseArray(){
+        return NULL;
+    }
+
+}
